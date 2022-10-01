cba4255b4d6afa974d2d8271232bd4d40d90fd0e
Vaishnavi <110771907+vaishnavigithu@users.noreply.github.com>
2022-10-01 13:39:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+let USERS_DB = [];
+
+ function validate(){
+    let firstName = document.getElementById('first-name').value;
+    let lastName  = document.getElementById('last-name').value;
+    let email  = document.getElementById('Email').value;
+    let password  = document.getElementById('Password').value;
+    let confirmPassword  = document.getElementById('Confirm-password').value;
+    // console.log(firstName,lastName,email,password,confirmPassword);
+
+    if(firstName.length>=2){
+        document.getElementById('first-name-valid').style.display ='block';
+        document.getElementById('first-name-invalid').style.display ='none';
+     }else{
+         document.getElementById('first-name-invalid').style.display ='block';
+         document.getElementById('first-name-valid').style.display ='none';
+      }
+      if(lastName.length>=2){
+        document.getElementById('last-name-valid').style.display ='block';
+        document.getElementById('last-name-invalid').style.display ='none';
+     }else{
+         document.getElementById('last-name-invalid').style.display ='block';
+         document.getElementById('last-name-valid').style.display ='none';
+      }
+    
+      if(email.includes('@') && 
+         email.includes('.') && 
+         email.indexOf("@") > 0 &&
+         email.substr(email.lastIndexOf('.')+1).length >=2){
+             document.getElementById('email-valid').style.display= 'block';
+             document.getElementById('email-invalid').style.display="none";
+         }else{
+             document.getElementById('email-invalid').style.display= 'block';
+             document.getElementById('email-valid').style.display="none";
+         }
+         let error=false
+     if(password !== confirmPassword){
+         document.getElementById('confirm-password-invalid').style.display='block';
+         error=true
+     }else{
+       document.getElementById('confirm-password-invalid').style.display='none';
+     }
+     if(!error){
+       let userDetails={
+          firstName,
+          lastName,
+          email,
+          password
+       }
+       USERS_DB.push(userDetails)
+       alert('Your details have been saved successfully');
+       document.getElementById('signup-form-id').reset();
+ 
+       console.log(USERS_DB);
+     }
+
+ }
